23b31b871d79088d7b5d8cc0fb8a392010271167
unknown <jellywase@naver.com>
2025-10-16 17:02:25 +0900

    Implemented FJFeedWatcher

--- TickerManageProgram/FJFeedWatcher.cs
+++ TickerManageProgram/FJFeedWatcher.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FinancialJuiceObserver;
+using LLMLibrary;
 
 namespace TickerManageProgram
 {
@@ -12,6 +13,8 @@ namespace TickerManageProgram
         static object lockObj = new object();
         static FeedWatcher feedWatcher = new();
         static CancellationTokenSource cts;
+        static ILLMClient llmClient => LLMProvider.defaultLLMClient;
+        enum FeedImportance { important, ordinary}
 
         static async Task WatchLoop(CancellationToken token)
         {
@@ -19,8 +22,10 @@ namespace TickerManageProgram
             {
                 while (!token.IsCancellationRequested)
                 {
-                    await feedWatcher.Update();
+                    var newFeeds = await feedWatcher.Update();
+                    await HandleNewFeeds(newFeeds);
 
+                    // FinancialJuice 조회 텀: 1분
                     await Task.Delay(60000, token);
                 }
             }
@@ -60,6 +65,57 @@ namespace TickerManageProgram
                 cts.Dispose();
                 cts = null;
             }
+        }
+
+        static async Task HandleNewFeeds(IEnumerable<NewFeedInfo> newFeeds)
+        {
+            int cnt = 0;
+            foreach (var newFeed in newFeeds)
+            {
+                string feedSummary = BuildFeedSummary(newFeed);
+
+                // LLM에게 중요도 분석의뢰
+                var importance = await AskImportanceToLLM(feedSummary, newFeed.pubDate.ToString() + cnt);
+                
+                // 중요도 높을 시 로그
+                if (importance is FeedImportance.important)
+                {
+                    LogChannel.EnqueueLog(new Log(Log.LogType.info, feedSummary));
+                }
+
+                cnt++;
+            }
+        }
+
+        static async Task<FeedImportance> AskImportanceToLLM(string feedSummary, string chatID)
+        {
+            const string systemMessage = "다음 소식을 듣고 미국의 주식과 경제에 큰 영향을 미칠경우 important라고만 답하고, 사소하다면 ordinary라고만 답해주세요.";
+            await llmClient.SetSystemMessage(chatID, systemMessage);
+
+            string importance = await llmClient.SendUserMessage(chatID, feedSummary);
+            await llmClient.DisposeChat(chatID);
+            switch (importance)
+            {
+                case "important":
+                    return FeedImportance.important;
+                    break;
+
+                case "ordinary":
+                    return FeedImportance.ordinary;
+                    break;
+
+                default:
+                    return FeedImportance.ordinary;
+                    break;
+            }
+        }
+        static string BuildFeedSummary(NewFeedInfo feedInfo)
+        {
+            StringBuilder sb = new();
+            sb.AppendLine($"보고일: {feedInfo.pubDate.ToString()}");
+            sb.AppendLine($"제목: {feedInfo.title}");
+            sb.Append($"내용: {feedInfo.description}");
+            return sb.ToString();
         }
     }
 }
