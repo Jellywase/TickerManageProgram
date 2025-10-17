e7426523f101911791e341c6c5537e5f56830dd1
Jellywase <jellywase@naver.com>
2025-10-17 12:07:31 +0900

    Updated LLMLibrary.dll, covered HandleNewFJFeed with try catch

--- TickerManageProgram/obj/Release/net8.0/TickerManageProgram.AssemblyInfo.cs
+++ TickerManageProgram/obj/Release/net8.0/TickerManageProgram.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TickerManageProgram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5013f1bd9ab5d68385e766d67ffecb11658585f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25cc71c6e159aa2718b91011e2e83214635f8ad5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TickerManageProgram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TickerManageProgram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
