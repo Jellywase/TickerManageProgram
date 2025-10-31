a6713ff92ee24621555b2b253acde5184c6dc470
Jellywase <jellywase@naver.com>
2025-11-01 00:53:01 +0900

    Include sln

--- TickerManageProgram/obj/Debug/net8.0/TickerManageProgram.AssemblyInfo.cs
+++ TickerManageProgram/obj/Debug/net8.0/TickerManageProgram.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TickerManageProgram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c415e91b9c5eac022ffaeb913a25ae7fdde08b5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c566c6b2a622d450fbe6861d6060353fc3fd3b41")]
 [assembly: System.Reflection.AssemblyProductAttribute("TickerManageProgram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TickerManageProgram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
